50c5dad6bd4bded18f0e17ea34af244c0b561ab2
Lenovo <Lenovo@host.docker.internal>
2022-09-23 13:55:14 +0530

    Spring boot Project template

---
+++ FizzBuzz_1/src/main/java/com/example/FizzBuzz_1/FizzBuzz1Application.java
@@ -0,0 +1,13 @@
+package com.example.FizzBuzz_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzBuzz1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzBuzz1Application.class, args);
+	}
+
+}
